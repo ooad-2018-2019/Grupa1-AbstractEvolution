8185fc540542084d24dedab4d65c8cfc695b19d5
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-06-17 16:16:21 +0200

    Osnovne DTO klase

---
+++ Implementacija/OOADProjekat/OOADProjekat/Modeli/Prodavac.cs
@@ -0,0 +1,9 @@
+namespace OOADProjekat.Modeli
+{
+    public class Prodavac : Korisnik
+    {
+        public Prodavac(string id, string imePrezime, string username, string password, string email) : base(id, imePrezime, username, password, email)
+        {
+        }
+    }
+}
---
+++ Implementacija/OOADProjekat/OOADProjekat/Modeli/Stan.cs
@@ -0,0 +1,9 @@
+namespace OOADProjekat.Modeli
+{
+    public class Stan : Nekretnina
+    {
+        public Stan(string id, string naziv, double cijena, Prodavac prodavac, string adresa, int kvadratura, string opis) : base(id, naziv, cijena, prodavac, adresa, kvadratura, opis)
+        {
+        }
+    }
+}
