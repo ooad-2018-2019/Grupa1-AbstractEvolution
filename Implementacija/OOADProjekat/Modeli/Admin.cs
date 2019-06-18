bd381bdb85975614e1ced90ce0b3aa002b9c4471
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-06-18 15:20:10 +0200

    Commit

---
+++ Implementacija/OOADProjekat/Modeli/Admin.cs
@@ -0,0 +1,9 @@
+namespace OOADProjekat.Modeli
+{
+    public class Admin : Korisnik
+    {
+        public Admin(string id, string imePrezime, string username, string password, string email) : base(id, imePrezime, username, password, email)
+        {
+        }
+    }
+}
---
+++ Implementacija/OOADProjekat/Modeli/Kuca.cs
@@ -0,0 +1,9 @@
+namespace OOADProjekat.Modeli
+{
+    public class Kuca : Nekretnina
+    {
+        public Kuca(string id, string naziv, double cijena, Prodavac prodavac, string adresa, int kvadratura, string opis) : base(id, naziv, cijena, prodavac, adresa, kvadratura, opis)
+        {
+        }
+    }
+}
